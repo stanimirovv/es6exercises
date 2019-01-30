51024d8cd1b61d6d2d21662d3a9eea325a20d184
Zlatin Stanimirov <stanimirovv@zlatin-mbp.bg.nc>
2019-01-30 14:21:16 +0200

    homework 1

---
+++ hw1/song.js
@@ -0,0 +1,24 @@
+/*
+ * Contains metadata for song Shape of you by Ed Sheeran
+ * Data was taken from: https://en.wikipedia.org/wiki/Shape_of_You
+ */
+
+// Writers and producers metadata
+var artist = "Ed Sheeran";
+var producers = [	"Ed Sheeran", "Steve Mac"	];
+var songWriters = [ "Ed Sheeran", "Steve Mac",  "Johnny McDaid", "Kandi Burruss", "Tameka Cottle", "Kevin Briggs" ];
+
+// Song specific metadata
+song = {
+genre : [ 'Pop', 'dancehall', 'tropical house' ],
+durationInSeconds : 232,
+releaseDate : '6 January 2017',
+yearOfRecording : '2016',
+hasVideo : true,
+albumName: 'Divide'
+};
+
+console.log(artist);
+console.log(producers);
+console.log(songWriters);
+console.log(song);
